f8173dbbd771fa2b59556fcce5c4f8183bfe6299
Mourad EL CADI <mouradface1998@gmail.com>
2023-03-09 00:53:42 +0100

    save

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
 mod client;
 mod server;
 
-use server::run;
-
 // use std::env;
 
-fn main() {
+async fn run() -> Result<(), String> {
+    let mut stdin = tokio::io::stdin();
+    let mut stdout = tokio::io::stdout();
+
+    tokio::spawn(async move {
+        tokio::io::copy(&mut stdin, &mut stdout).await.unwrap();
+    }).await.unwrap();
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    run().await.unwrap();
+    
     /*
         let args: Vec<_> = env::args().collect();
         if args.len() > 1 {
             println!("The first argument is {}", args[1]);
         }
     */
-
-    run("127.0.0.1", 3000).unwrap();
 }
