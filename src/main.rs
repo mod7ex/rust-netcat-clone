c19a6a538fcd8cb3a421f1f5f522608565c324c0
Mourad EL CADI <mouradface1998@gmail.com>
2023-03-11 23:56:05 +0100

    save

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod client;
 mod server;
 mod stream;
-/* mod common; */
-/* mod tls; */
+mod common;
+mod tls;
 
-use std::{ops::RangeInclusive, time::Duration};
+use std::{ops::RangeInclusive, time::Duration, path::Path};
 use clap::{Parser, Subcommand};
-// use std::env;
-use stream::{ client, server };
+/* use stream::{ client, server }; */
+use tls::{tls_connect, tls_listen};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -25,6 +25,9 @@ enum Command {
 
         #[arg(short, long, value_parser = port_in_range)]
         port: u16,
+
+        #[arg(long, value_parser = valid_path)]
+        ca: Option<String>
     },
 
     /// Start a server
@@ -34,6 +37,15 @@ enum Command {
 
         #[arg(short, long, value_parser = port_in_range)]
         port: u16,
+
+        #[arg(long, value_parser = valid_path)]
+        ca: Option<String>,
+
+        #[arg(long, value_parser = valid_path)]
+        cert: Option<String>,
+
+        #[arg(long, value_parser = valid_path)]
+        key: Option<String>,
     },
 }
 
@@ -54,33 +66,53 @@ fn port_in_range(s: &str) -> Result<u16, String> {
     }
 }
 
+fn valid_path(s: &str) -> Result<String, String> {
+    let path = Path::new(s);
+
+    if path.exists() {
+        Ok(s.to_string())
+    } else {
+        Err(format!("Path does not exist {}",s))
+    }
+}
+
 fn main() {
     let cli = Cli::parse();
 
     let runtime = tokio::runtime::Runtime::new().unwrap();
 
     match cli.command {
-        Command::Connect { host, port } => {
+        Command::Connect { host, port, ca } => {
             println!("connect to {}:{}", host, port);
 
             runtime.block_on(async {
                 tokio::select! {
-                    _ = client() => {}
+                    res = tls_connect(&host, port, ca) => {
+                        if let Err(e) = res {
+                            println!("connect failed: {}", e.to_string());
+                        }
+                    }
+                    /* _ = client() => {} */
                     _ = tokio::signal::ctrl_c() => {}
                 }
             });
         }
-        Command::Serve { bind_host, port } => {
+        Command::Serve { bind_host, port, ca, cert, key } => {
             println!("bind to {}:{}", bind_host, port);
 
             runtime.block_on(async {
                 tokio::select! {
-                    _ = server() => {}
+                    /* _ = server() => {} */
+                    res = tls_listen(&bind_host, port, ca, cert.clone().expect("cert is required"), key.clone().expect("key is required")) => {
+                        if let Err(e) = res {
+                            println!("listen failed: {}", e.to_string());
+                        }
+                    }
                     _ = tokio::signal::ctrl_c() => {}
                 }
             });
         }
     }
 
-    runtime.shutdown_timeout(Duration::from_secs(0));
+    runtime.shutdown_timeout(Duration::from_secs(5));
 }
--- src/tls.rs
+++ src/tls.rs
@@ -1,17 +1,21 @@
-use std::fs::File;
-use std::io;
-use std::io::{BufReader, Error};
-use std::path::Path;
-use std::sync::Arc;
-use tokio::io::split;
-use tokio::net::{TcpListener, TcpStream};
-use tokio_rustls::{TlsConnector, rustls::ClientConfig, TlsAcceptor};
-use tokio_rustls::rustls::{Certificate, OwnedTrustAnchor, PrivateKey, RootCertStore, ServerConfig};
 use webpki_roots;
-use crate::common::read_write;
 use rustls_pemfile;
+use crate::common::read_write;
+use std::{
+    fs::File, 
+    io::{self, BufReader, Error},
+    path::Path,
+    sync::Arc
+};
+use tokio::{io::split, net::{TcpListener, TcpStream}};
+use tokio_rustls::{
+    TlsConnector,
+    rustls::ClientConfig,
+    TlsAcceptor,
+    rustls::{Certificate, OwnedTrustAnchor, PrivateKey, RootCertStore, ServerConfig}
+};
 
-pub async fn tls_connect(host: &String, port: &u16) -> Result<(), Error> {
+pub async fn tls_connect(host: &str, port: u16, ca: Option<String>) -> Result<(), Error> {
     let addr = format!("{}:{}", host, port);
 
     let mut root_cert_store = RootCertStore::empty();
@@ -25,6 +29,16 @@ pub async fn tls_connect(host: &String, port: &u16) -> Result<(), Error> {
         },
     ));
 
+    if let Some(ca) = ca {
+        for cert in load_certs(Path::new(&ca)).unwrap() {
+            root_cert_store.add(&cert)
+                .map_err(|_e| {
+                    Error::new(io::ErrorKind::InvalidInput, "couldn't add CA")
+                })
+                .unwrap();
+        }
+    }
+
     let config = ClientConfig::builder()
         .with_safe_defaults()
         .with_root_certificates(root_cert_store)
@@ -32,7 +46,7 @@ pub async fn tls_connect(host: &String, port: &u16) -> Result<(), Error> {
 
     let tls_connector = TlsConnector::from(Arc::new(config));
 
-    let server_name = host.as_str().try_into().unwrap();
+    let server_name = host.try_into().unwrap();
 
     let stream = TcpStream::connect(&addr).await?;
     let stream = tls_connector.connect(server_name, stream).await?;
@@ -44,3 +58,64 @@ pub async fn tls_connect(host: &String, port: &u16) -> Result<(), Error> {
     Ok(())
 }
 
+pub async fn tls_listen(host: &str, port: u16, ca: Option<String>, cert: String, key: String) -> Result<(), Error> {
+    let addr = format!("{}:{}", host, port);
+
+    let mut root_cert_store = RootCertStore::empty();
+    root_cert_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(
+        |ta| {
+            OwnedTrustAnchor::from_subject_spki_name_constraints(
+                ta.subject,
+                ta.spki,
+                ta.name_constraints,
+            )
+        },
+    ));
+
+    if let Some(ca) = ca {
+        for cert in load_certs(Path::new(&ca))? {
+            root_cert_store.add(&cert)
+                .map_err(|_e| {
+                    Error::new(io::ErrorKind::InvalidInput, "couldn't add CA")
+                })?;
+        }
+    }
+
+    let certs = load_certs(Path::new(cert.as_str()))?;
+    let mut keys = load_keys(Path::new(key.as_str()))?;
+    let config = ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth()
+        .with_single_cert(certs, keys.remove(0))
+        .map_err(|err| Error::new(io::ErrorKind::InvalidInput, err))?;
+
+    let acceptor = TlsAcceptor::from(Arc::new(config));
+
+    let listener = TcpListener::bind(&addr).await?;
+
+    let (socket, _) = listener.accept().await?;
+
+    let stream = acceptor.accept(socket).await?;
+
+    let (reader, writer) = split(stream);
+
+    read_write(reader, writer).await;
+
+    Ok(())
+}
+
+fn load_certs(path: &Path) -> io::Result<Vec<Certificate>> {
+    let f = File::open(path)?;
+
+    return rustls_pemfile::certs(&mut BufReader::new(f))
+        .map_err(|_| Error::new(io::ErrorKind::InvalidInput, "invalid cert"))
+        .map(|mut certs| certs.drain(..).map(Certificate).collect());
+}
+
+fn load_keys(path: &Path) -> io::Result<Vec<PrivateKey>> {
+    let f = File::open(path)?;
+
+    rustls_pemfile::rsa_private_keys(&mut BufReader::new(f))
+        .map_err(|_| Error::new(io::ErrorKind::InvalidInput, "invalid key"))
+        .map(|mut keys| keys.drain(..).map(PrivateKey).collect())
+}
